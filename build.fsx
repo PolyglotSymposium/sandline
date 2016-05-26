9cdde4e791a68930ec143f5f840da1c899f89218
Keith Pinson <kazark@zoho.com>
2016-05-25 21:49:09 -0400

    Introduce spec project

--- build.fsx
+++ build.fsx
@@ -12,12 +12,26 @@ let compileSrcSln () =
     |> MSBuildRelease buildDir "Build"
     |> Log "Compile-Output: "
 
+let runFuchuTests() =
+    let errorCode =
+        !! (buildDir </> "*.spec.exe")
+        |> Seq.map (fun p -> if not isMono then p,null else "mono",p)
+        |> Seq.map (fun (p,a) -> asyncShellExec { defaultParams with Program = p; CommandLine = a })
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.sum
+    if errorCode <> 0
+    then failwith "Unit tests failed"
+
 Target "clean" clean
 Target "compile" compileSrcSln
+Target "test" runFuchuTests
 Target "rebuild" DoNothing
 
 "clean" ==> "rebuild"
 "clean" ?=> "compile"
 "compile" ==> "rebuild"
+"compile" ?=> "test"
+"test" ==> "rebuild"
 
 RunTargetOrDefault "rebuild"
---
+++ src/sandline.spec/Specs.fs
@@ -0,0 +1,11 @@
+﻿module Specs
+
+open Fuchu
+open Swensen.Unquote
+
+[<Tests>]
+let specs = 
+    testList "Bootstrap" [
+        testCase "Bootstrap" <| fun _ ->
+            test <@ true @>
+    ]
